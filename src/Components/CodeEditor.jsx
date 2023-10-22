beee856dd7c2d3aca1e726bb8d879ff14ef5cc4a
tanishaAngra <tanishaangra1511@gmail.com>
2023-10-23 01:13:57 +0530

    new changes

--- src/Components/CodeEditor.jsx
+++ src/Components/CodeEditor.jsx
@@ -1,11 +1,9 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./CodeEditor.css";
- 
 
 const CodeEditor = () => {
   const [code, setCode] = useState("//Write your code here");
   const [locked, setLocked] = useState(false);
-  
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(code);
@@ -27,12 +25,12 @@ const CodeEditor = () => {
           onChange={(e) => setCode(e.target.value)}
           readOnly={locked}
         />
-      
-      <div className="button-container">
-        <button onClick={copyToClipboard}>Copy</button>
-        <button onClick={lockCode}>{locked ? "Unlock" : "Lock"}</button>
-        <button onClick={saveCode}>Save</button>
-      </div>
+
+        <div className="button-container">
+          <button onClick={copyToClipboard}>Copy</button>
+          <button onClick={lockCode}>{locked ? "Unlock" : "Lock"}</button>
+          <button onClick={saveCode}>Save</button>
+        </div>
       </div>
     </div>
   );
